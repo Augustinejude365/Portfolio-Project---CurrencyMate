b5dda35e7ae36079a68a5fece07e506a90146e37
Emmanuel Ademola <mailemmydee@gmail.com>
2024-04-24 23:23:14 +0100

    updated: API base URL & organisation

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
+
+""" DON'T DELETE!!!!
+shebang for my local windows venv
+#!/c/Users/TOSHIBA/Documents/Portfolio-Project---CurrencyMate/my-venv/Scripts/python
+"""
+
 from flask import Flask, render_template, request, jsonify
 import requests
 
 app = Flask(__name__)
-
-
-def get_exchange_rate(from_currency, to_currency):
-    url = f"https://api.exchangerate-api.com/v4/latest/{from_currency}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        return data["rates"].get(to_currency)
-    else:
-        return None
 
 
 @app.route("/")
@@ -40,6 +36,16 @@ def convert_currency(amount, from_currency, to_currency):
     else:
         return "Unable to perform conversion. Please check the currencies\
                  provided."
+
+
+def get_exchange_rate(from_currency, to_currency):
+    url = f"https://open.er-api.com/v6/latest/{from_currency}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data["rates"].get(to_currency)
+    else:
+        return None
 
 
 if __name__ == "__main__":
